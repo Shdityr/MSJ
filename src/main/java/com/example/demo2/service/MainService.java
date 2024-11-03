56f88c1e78b987b77d80bccdf06b4adbd50c6686
nono123674 <13183338039@163.com>
2024-11-03 21:18:28 +0800

    Update MainService.java

--- src/main/java/com/example/demo2/service/MainService.java
+++ src/main/java/com/example/demo2/service/MainService.java
@@ -39,10 +39,10 @@ public class MainService {
     ImageRepository ImageRepository;
 
     public void func1(){
-        ImageRepository.deleteAll();
-        // RestaurantEntity shop = RestaurantRepository.findById(1).get();
-        // shop.setImages("3");
-        // RestaurantRepository.save(shop);
+        // ImageRepository.deleteAll();
+        RestaurantEntity shop = RestaurantRepository.findById(1).get();
+        shop.setImages("1");
+        RestaurantRepository.save(shop);
     }
     public void test1(){
         int i1=1;
